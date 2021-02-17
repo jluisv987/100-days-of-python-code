85f3d22822a45efdc35a3f8ad062410c51332af0
jluisv987 <jluisv987@gmail.com>
2021-02-16 20:45:12 -0600

    Modificado día 16

--- Day 16/main.py
+++ Day 16/main.py
@@ -1,3 +1,24 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_make = CoffeeMaker()
+money_machine = MoneyMachine()
+is_on = True
+menu = Menu()
+print("Welcome to the coffee machine")
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? {options}:")
+    if choice == "off":
+        is_on = False
+    elif choice =="report":
+        money_machine.report()
+        coffee_make.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_make.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_make.make_coffee(drink)
+
